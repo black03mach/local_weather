54a355e8bd7ec5e53de814df5d050a9762f83dbb
Patrick Wells <pat.taylorwells@gmail.com>
2020-07-09 17:56:31 -0400

    fixed

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 var cityInput = document.getElementById("cityInput").value;
-console.log(cityInput)
 var APIKey = "56aab3faba12a50138eb8a46ffc3825b";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+
 
 $('button').on('click', function () {
     event.preventDefault();
-    cityInput.name = $("#cityInput").val().trim()
+    cityInput = $("#cityInput").val().trim()
+    console.log(cityInput);
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + APIKey;
+
     $.ajax({
         url : queryURL,
         method : "GET"
     }).then(function(response){
+        console.log(response)
+    
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+        $(".city").html("<h2>" + response.name + " Weather Details</h2>")
+        var tempF = ((response.main.temp -273.15) * 1.80 + 32);
+        var fixedF = Math.round(tempF)
+        $(".temp").html("<h3>" + fixedF + " degrees Fahrenheit")
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
         })
-
 })
